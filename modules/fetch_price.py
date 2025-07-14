65dce95932d06ec032d61bf09c48d58dbf5f70a2
morlayo232 <lty232@naver.com>
2025-07-14 19:53:36 +0900

    Update fetch_price.py

--- modules/fetch_price.py
+++ modules/fetch_price.py
@@ -4,7 +4,6 @@ from modules.fetch_naver import get_naver_price
 from modules.fetch_daum import get_daum_price
 
 def fetch_stock_price(code):
-    # 1. 야후 파이낸스 시도
     try:
         df = yf.download(f"{code}.KS", period="6mo", progress=False)
         if not df.empty:
@@ -13,14 +12,12 @@ def fetch_stock_price(code):
             return df[["Date", "Close", "Open", "High", "Low", "Volume"]]
     except Exception:
         pass
-    # 2. 네이버
     try:
         df = get_naver_price(code)
         if not df.empty:
             return df
     except Exception:
         pass
-    # 3. 다음
     try:
         df = get_daum_price(code)
         if not df.empty:
