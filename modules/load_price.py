d187221e9b707dc388594ac8885090e7295faa21
morlayo232 <lty232@naver.com>
2025-07-15 06:33:49 +0900

    Update load_price.py

--- modules/load_price.py
+++ modules/load_price.py
@@ -1,4 +1,8 @@
 from pykrx import stock
 
-def load_price_history(code, start="20240101", end="20250714"): try: df = stock.get_market_ohlcv_by_date(start, end, code) return df except: return None
-
+def load_price_history(code, start="20240101", end="20250714"):
+    try:
+        df = stock.get_market_ohlcv_by_date(start, end, code)
+        return df
+    except:
+        return None
