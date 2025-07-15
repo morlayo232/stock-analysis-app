59dafb236d08dee87320a3ace86dad9b3b53c208
morlayo232 <lty232@naver.com>
2025-07-15 19:33:32 +0900

    Update update_stock_database.py

--- update_stock_database.py
+++ update_stock_database.py
@@ -1,5 +1,10 @@
-import pandas as pd
+# 📄 update_stock_database.py
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import pandas as pd
 from modules.fetch_price import fetch_price
 from modules.fetch_naver import fetch_naver_fundamentals
 from modules.calculate_indicators import add_tech_indicators
