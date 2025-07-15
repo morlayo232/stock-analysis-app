8a2dc041927babbd551691476f3a6196b3e435d8
morlayo232 <lty232@naver.com>
2025-07-15 13:13:00 +0900

    Update fetch_naver.py

--- modules/fetch_naver.py
+++ modules/fetch_naver.py
@@ -2,7 +2,7 @@ def fetch_naver_financials(code):
     # (예시: BeautifulSoup로 네이버에서 PER/PBR/ROE/배당 파싱, 실패시 모두 None 반환)
     try:
         # 실제 구현 필요
-        fin = {"PER": None, "PBR": None, "ROE": None, "배당수익률": None}
+        fin = {"PER": None, "PBR": None, "ROE": None, "배당률": None}
         return fin
     except Exception:
-        return {"PER": None, "PBR": None, "ROE": None, "배당수익률": None}
+        return {"PER": None, "PBR": None, "ROE": None, "배당률": None}
