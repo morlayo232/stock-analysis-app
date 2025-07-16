6665f7b7f3d2740b6f5d1cd13638d6d045afafd5
morlayo232 <lty232@naver.com>
2025-07-16 18:37:13 +0900

    Update score_utils.py

--- modules/score_utils.py
+++ modules/score_utils.py
@@ -1,7 +1,7 @@
-# modules/score_utils.py
+# score_utils.py
 
-import numpy as np
 import pandas as pd
+import numpy as np
 
 DEFAULT_FIN = ['PER', 'PBR', 'EPS', 'BPS', '배당률']
 
@@ -34,39 +34,26 @@ def finalize_scores(df, style="aggressive"):
     for col in DEFAULT_FIN:
         df[f'z_{col}'] = safe_zscore(df[col])
 
-    # 급등점수 z점수
-    if "급등점수" in df.columns:
-        df['z_급등점수'] = safe_zscore(df["급등점수"])
-    else:
-        df['z_급등점수'] = 0
-
     if style == "aggressive":
         score = (
-            -df['z_PER'] * 0.15 +
-            -df['z_PBR'] * 0.15 +
-            df['z_EPS'] * 0.15 +
-            df['z_BPS'] * 0.10 +
-            df['z_배당률'] * 0.10 +
-            df['z_급등점수'] * 0.20 +
-            df['z_EPS'] * 0.15  # 성장성 추가 비중
+            -df['z_PER'] * 0.25 +
+            -df['z_PBR'] * 0.25 +
+            df['z_EPS'] * 0.25 +
+            df['z_BPS'] * 0.15 +
+            df['z_배당률'] * 0.10
         )
     elif style == "stable":
         score = (
-            -df['z_PER'] * 0.15 +
-            -df['z_PBR'] * 0.25 +
-            df['z_BPS'] * 0.20 +
-            df['z_배당률'] * 0.20 +
-            df['z_급등점수'] * 0.10 +
-            df['z_EPS'] * 0.10
+            -df['z_PER'] * 0.2 +
+            -df['z_PBR'] * 0.3 +
+            df['z_BPS'] * 0.3 +
+            df['z_배당률'] * 0.2
         )
     elif style == "dividend":
         score = (
-            df['z_배당률'] * 0.40 +
-            -df['z_PBR'] * 0.15 +
-            -df['z_PER'] * 0.10 +
-            df['z_BPS'] * 0.10 +
-            df['z_급등점수'] * 0.10 +
-            df['z_EPS'] * 0.15
+            df['z_배당률'] * 0.6 +
+            -df['z_PBR'] * 0.2 +
+            -df['z_PER'] * 0.2
         )
     else:
         score = np.zeros(len(df))
