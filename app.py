501957af5e47e0794cdd94d44a929501fa521089
morlayo232 <lty232@naver.com>
2025-07-20 17:52:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,25 +19,15 @@ from pykrx import stock
 
 st.set_page_config(page_title="Investment Manager", layout="wide")
 
-# 로고와 타이틀을 가로로 배치하고 스타일 조정
-col_logo, col_title = st.columns([1, 2])
 
-with col_logo:
-    try:
-        logo_img = Image.open("logo_tynex.png")
-        st.image(logo_img, width=150)
-    except Exception:
-        st.write("로고 이미지 로드 실패")
+# 로고를 중앙에 크게 배치하고 타이틀 이름은 삭제
+try:
+    logo_img = Image.open("logo_tynex.png")
+    st.image(logo_img, width=300, use_column_width=False)
+except Exception:
+    st.write("로고 이미지 로드 실패")
 
-with col_title:
-    st.markdown(
-        """
-        <h3 style='color:#0055a5; font-family:"Segoe UI", Tahoma, Geneva, Verdana, sans-serif; margin-top:0px;'>
-            Investment Manager
-        </h3>
-        """, 
-        unsafe_allow_html=True
-    )
+
 @st.cache_data(ttl=3600, show_spinner=False)
 def load_filtered_data():
     try:
