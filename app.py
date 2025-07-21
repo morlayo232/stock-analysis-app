a7f1a1067c7b5c48cfbe0cec4ebb681ad8e7c1aa
morlayo232 <lty232@naver.com>
2025-07-21 22:04:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,7 @@ from PIL import Image
 
 
 sys.path.append(os.path.abspath("modules"))
-# app.py 상단 임포트에 추가
-from evaluate_stock import evaluate_stock(scored_df, selected, df_price)
-
+from evaluate_stock import evaluate_stock
 from score_utils import finalize_scores, assess_reliability
 from fetch_news import fetch_google_news
 from chart_utils import plot_price_rsi_macd
